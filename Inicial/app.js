0af5e186b0288f1bdb257762154273ca9296ed9e
KaikeVitorino <kaikevoliveira@gmail.com>
2023-11-09 15:00:46 -0300

    aaaaaaaa aaaaaaaaaaa

--- Inicial/app.js
+++ Inicial/app.js
@@ -9,7 +9,7 @@ switcher.addEventListener('click', function(){
     // Alterna a classe 'dark-theme' no elemento <body>aa
     document.body.classList.toggle('dark-theme');
 
-    // Obtém a lista de classes do elemento <body>aaaaaaaaaaaaaa
+    // Obtém a lista de classes do elemento <body>aaaaaaaaaaaaaaa
     var className = document.body.className;
 
     // Verifica se a classe 'light-theme' está presenteaaa
