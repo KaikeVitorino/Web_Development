ecb9d664e0df636eb6221e96389e49aa6b430164
KaikeVitorino <kaikevoliveira@gmail.com>
2023-11-10 15:28:38 -0300

    aaaaaaaa aaaaaaaaaaaa

--- Inicial/app.js
+++ Inicial/app.js
@@ -6,7 +6,7 @@ let direcao = 1;
 
 // Adiciona um ouvinte de evento de clique no botãooa
 switcher.addEventListener('click', function(){
-    // Alterna a classe 'dark-theme' no elemento <body>aaaaaa
+    // Alterna a classe 'dark-theme' no elemento <body>aaaaaaa
     document.body.classList.toggle('dark-theme');
 
     // Obtém a lista de classes do elemento <body>aaaaaaaaaaaaaaa
