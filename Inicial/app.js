932f2ee3955860bc0ceb4281538c31139f71e11a
KaikeVitorino <kaikevoliveira@gmail.com>
2023-11-10 15:27:41 -0300

    aaaaaaaa aaaaaaaaaaaa

--- Inicial/app.js
+++ Inicial/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Seleciona o botão com a classe .btn e atribui à variável switcheraaaaaaa
+// Seleciona o botão com a classe .btn e atribui à variável switcheraaaaaaaa
 const switcher = document.querySelector('.btn');
 let direcao = 1;
 
